1cefccd8e1387f2e96b6dfa6e4227c27df564c96
Varphone Wong <varphone@qq.com>
2016-11-07 10:28:57 +0800

    lots of changes

--- board/tdc/etlmd-v2/run.sh
+++ board/tdc/etlmd-v2/run.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 DESTDIR=/opt/tdc/etlmd-v2
-BOOTARGS="-append 'root=/dev/ram0 overlayroot=/dev/sdb4:rw:ext2 console=ttyS0,115200'"
-UDISK="-usbdevice disk:format=raw:/tmp/etlmd-v2-2g.raw"
-echo "BOOTARGS ${BOOTARGS}"
-LC_ALL=C qemu-system-x86_64 -M pc -m 1024M -kernel ${DESTDIR}/images/bzImage -initrd ${DESTDIR}/images/initrd.cpio.gz -hda ${DESTDIR}/images/rootfs.ext2 -hdb /tmp/etlmd-v2-8g.raw -device e1000,netdev=network0,mac=68:22:33:44:55:AF -netdev tap,id=network0,ifname=tap0,script=no,downscript=no ${UDISK} -append 'root=/dev/ram0 overlayroot=/dev/sdb4:rw:ext2 console=ttyS0,115200' -nographic  
+KERNEL="-kernel ${DESTDIR}/images/bzImage"
+INITRD="-initrd ${DESTDIR}/images/initrd.cpio.gz"
+BOOTARGS="-append root=/dev/ram0 -append overlayroot=/dev/sdb4:rw:ext2 -append console=ttyS0,115200"
+DISK1="-hda ${DESTDIR}/images/rootfs.ext2"
+DISK2="-hdb ${HOME}/workspace/tests/etlmdv2-8g.raw"
+DISK3="-usbdevice disk:format=raw:${HOME}/workspace/tests/etlmdv2-2g.raw"
+NETIF="-device e1000,netdev=network0,mac=68:22:33:44:55:AF -netdev tap,id=network0,ifname=tap0,script=no,downscript=no"
+LANG=en
+EXECARGS="${KERNEL} ${INITRD} ${BOOTARGS} ${DISK1} ${DISK2} ${DISK3} ${NETIF}"
+EXEC="qemu-system-x86_64 -M pc -m 1024M -nographic ${EXECARGS}"
+
+echo ${EXEC}
+${EXEC}
+#qemu-system-x86_64 -M pc -m 1024M -append 'root=/dev/sdb4 overlayroot=/dev/sdb5:rw:ext2 console=ttyS0,115200' -nographic  
