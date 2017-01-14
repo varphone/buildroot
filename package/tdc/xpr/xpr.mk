9aec87f7e0a9f7e0a26fea859361eb513f1bf4e1
黄华锋 <varphone@qq.com>
2017-01-14 14:35:43 +0800

    ppmd-3531,xpr: Update Kbuild

--- package/tdc/xpr/xpr.mk
+++ package/tdc/xpr/xpr.mk
@@ -4,22 +4,32 @@
 #
 ################################################################################
 
-XPR_VERSION := $(call qstrip, $(BR2_PACKAGE_TDC_XPR_VERSION))
+ifeq ($(BR2_PACKAGE_XPR_CUSTOM_VERSION),y)
+XPR_VERSION = $(call qstrip, $(BR2_PACKAGE_XPR_CUSTOM_VERSION_VALUE))
+else
+XPR_VERSION = 2.0
+endif
 XPR_SOURCE = xpr-$(XPR_VERSION).tar.bz2
 #XPR_SITE = https://10.0.2.2/cgit/rdst/binaries-release.git/plain
 #XPR_SITE = https://10.0.2.2/git/rdst/binaries-release/raw/master
 #XPR_SITE = https://10.0.2.2/git/rdst/ppmd-3531/archive
-XPR_SITE = https://a000:a000,,100200@10.0.2.2/cgit/rdst/xpr.git/snapshot
+XPR_SITE = https://10.0.2.2/cgit/rdst/xpr.git/snapshot
 XPR_STRIP_COMPONENTS = 1
 XPR_INSTALL_STAGING = YES
 XPR_LICENSE = GPLv2
 XPR_LICENSE_FILES = COPYING
 
+XPR_MAKE_ENV = \
+ CROSS_COMPILER_PREFIX=$(TARGET_CROSS)\
+ BUILD_DOCS=n\
+ BUILD_EXAMPLES=n\
+ BUILD_TESTS=n
+
 define XPR_CONFIGURE_CMDS
 endef
 
 define XPR_BUILD_CMDS
-	$(MAKE) -j1 CROSS_COMPILER_PREFIX=$(TARGET_CROSS) BUILD_DOCS=n BUILD_EXAMPLES=n BUILD_TESTS=n -C $(@D) all
+	$(MAKE1) $(XPR_MAKE_ENV) -C $(@D) all
 endef
 
 define XPR_INSTALL_STAGING_CMDS
@@ -32,5 +42,4 @@ define XPR_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/lib/libxpr.so $(TARGET_DIR)/usr/lib
 endef
 
-#$(eval $(kernel-module))
 $(eval $(generic-package))
