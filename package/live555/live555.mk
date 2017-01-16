a488a7495088cbfcbf62c9d583709111f7a08dd9
黄华锋 <varphone@qq.com>
2017-01-16 19:18:18 +0800

    live555: Add custom tarball and force static supports

--- package/live555/live555.mk
+++ package/live555/live555.mk
@@ -4,16 +4,32 @@
 #
 ################################################################################
 
+ifeq ($(BR2_PACKAGE_LIVE555_CUSTOM_TARBALL),y)
+LIVE555_TARBALL = $(call qstrip,$(BR2_PACKAGE_LIVE555_CUSTOM_TARBALL_LOCATION))
+LIVE555_SITE = $(patsubst %/,%,$(dir $(LIVE555_TARBALL)))
+LIVE555_SOURCE = $(notdir $(LIVE555_TARBALL))
+LIVE555_STRIP_COMPONENTS = $(BR2_PACKAGE_LIVE555_CUSTOM_TARBALL_STRIP_COMPONENTS)
+BR_NO_CHECK_HASH_FOR += $(LIVE555_SOURCE)
+else
 LIVE555_VERSION = 2017.10.28
 LIVE555_SOURCE = live.$(LIVE555_VERSION).tar.gz
 LIVE555_SITE = http://www.live555.com/liveMedia/public
+endif
 LIVE555_LICENSE = LGPL-2.1+
 LIVE555_LICENSE_FILES = COPYING
 LIVE555_INSTALL_STAGING = YES
 
 LIVE555_CFLAGS = $(TARGET_CFLAGS)
 
+LIVE555_STATIC_LIBS =
+
+ifeq ($(BR2_PACKAGE_LIVE555_FORCE_STATIC_LIBS),y)
+LIVE555_STATIC_LIBS = y
+endif
 ifeq ($(BR2_STATIC_LIBS),y)
+LIVE555_STATIC_LIBS = y
+endif
+ifeq ($(LIVE555_STATIC_LIBS),y)
 LIVE555_CONFIG_TARGET = linux
 LIVE555_LIBRARY_LINK = $(TARGET_AR) cr
 else
@@ -46,10 +62,17 @@ endef
 
 define LIVE555_INSTALL_STAGING_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(STAGING_DIR) -C $(@D) install
+	$(INSTALL) -d -m 0755 $(STAGING_DIR)/usr/include/live
+	$(INSTALL) -D -m 0644 $(STAGING_DIR)/usr/include/BasicUsageEnvironment/*.* $(STAGING_DIR)/usr/include/live
+	$(INSTALL) -D -m 0644 $(STAGING_DIR)/usr/include/groupsock/*.* $(STAGING_DIR)/usr/include/live
+	$(INSTALL) -D -m 0644 $(STAGING_DIR)/usr/include/liveMedia/*.* $(STAGING_DIR)/usr/include/live
+	$(INSTALL) -D -m 0644 $(STAGING_DIR)/usr/include/UsageEnvironment/*.* $(STAGING_DIR)/usr/include/live
 endef
 
+ifeq ($(BR2_PACKAGE_LIVE555_FORCE_STATIC_LIBS),)
 define LIVE555_INSTALL_TARGET_CMDS
 	$(TARGET_MAKE_ENV) $(MAKE) DESTDIR=$(TARGET_DIR) PREFIX=/usr -C $(@D) install
 endef
+endif
 
 $(eval $(generic-package))
