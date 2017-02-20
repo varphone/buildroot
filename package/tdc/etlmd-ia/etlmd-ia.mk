f288968c5f22ee6e5df7eeec23ac6ba1daa9dfe5
黄华锋 <varphone@qq.com>
2017-02-20 16:55:59 +0800

    package: new etlmd-ia

---
+++ package/tdc/etlmd-ia/etlmd-ia.mk
@@ -0,0 +1,45 @@
+################################################################################
+#
+# etlmd-ia
+#
+################################################################################
+
+ifeq ($(BR2_PACKAGE_ETLMD_IA_CUSTOM_VERSION),y)
+ETLMD_IA_VERSION = $(call qstrip, $(BR2_PACKAGE_ETLMD_IA_CUSTOM_VERSION_VALUE))
+else
+ETLMD_IA_VERSION = 2.0.0
+endif
+ETLMD_IA_SOURCE = etlmd-ia-$(ETLMD_IA_VERSION).tar.bz2
+#ETLMD_IA_SITE = https://10.0.2.2/cgit/rdst/binaries-release.git/plain
+#ETLMD_IA_SITE = https://10.0.2.2/git/rdst/binaries-release/raw/master
+#ETLMD_IA_SITE = https://10.0.2.2/git/rdst/etlmd-ia/archive
+ETLMD_IA_SITE = https://10.0.2.2/cgit/rdst/etlmd-ia.git/snapshot
+ETLMD_IA_STRIP_COMPONENTS = 1
+ETLMD_IA_INSTALL_STAGING = NO
+ETLMD_IA_DEPENDENCIES = xpr
+ETLMD_IA_LICENSE = GPLv2
+ETLMD_IA_LICENSE_FILES = COPYING
+
+#ETLMD_IA_MAKE_ENV = CROSS_COMPILER_PREFIX=$(TARGET_CROSS)
+
+CP	= @cp
+MV	= @mv
+RMDIR	= @rmdir
+
+define ETLMD_IA_CONFIGURE_CMDS
+endef
+
+#$(MAKE1) $(ETLMD_IA_MAKE_ENV) -C $(@D)/src all
+define ETLMD_IA_BUILD_CMDS
+	$(MAKE) -C $(@D)/src all
+endef
+
+define ETLMD_IA_INSTALL_STAGING_CMDS
+endef
+
+define ETLMD_IA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/src/etlmd-ia $(TARGET_DIR)/usr/bin
+endef
+
+#$(eval $(kernel-module))
+$(eval $(generic-package))
