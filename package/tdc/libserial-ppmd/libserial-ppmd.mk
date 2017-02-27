acc26a666367dff07805561fcc08ac22b4af36d9
黄华锋 <varphone@qq.com>
2017-02-27 16:09:34 +0800

    libserial-ppmd: Update make target and env

--- package/tdc/libserial-ppmd/libserial-ppmd.mk
+++ package/tdc/libserial-ppmd/libserial-ppmd.mk
@@ -10,9 +10,6 @@ else
 LIBSERIAL_PPMD_VERSION = 1.0.1
 endif
 LIBSERIAL_PPMD_SOURCE = libserial-$(LIBSERIAL_PPMD_VERSION).tar.bz2
-#LIBSERIAL_PPMD_SITE = https://10.0.2.2/cgit/rdst/binaries-release.git/plain
-#LIBSERIAL_PPMD_SITE = https://10.0.2.2/git/rdst/binaries-release/raw/master
-#LIBSERIAL_PPMD_SITE = https://10.0.2.2/git/rdst/ppmd-3531/archive
 LIBSERIAL_PPMD_SITE = https://10.0.2.2/cgit/rdst/libserial.git/snapshot
 LIBSERIAL_PPMD_STRIP_COMPONENTS = 1
 LIBSERIAL_PPMD_INSTALL_STAGING = YES
@@ -35,15 +32,15 @@ define LIBSERIAL_PPMD_CONFIGURE_CMDS
 endef
 
 define LIBSERIAL_PPMD_BUILD_CMDS
-	$(MAKE1) $(LIBSERIAL_PPMD_MAKE_ENV) -C $(@D) all
+	$(MAKE1) $(LIBSERIAL_PPMD_MAKE_ENV) -C $(@D) clean all
 endef
 
 define LIBSERIAL_PPMD_INSTALL_STAGING_CMDS
-	$(MAKE) -C $(@D) install DESTDIR=$(STAGING_DIR) PREFIX=/usr
+	$(MAKE1) $(LIBSERIAL_PPMD_MAKE_ENV) -C $(@D) install DESTDIR=$(STAGING_DIR) PREFIX=/usr
 endef
 
 define LIBSERIAL_PPMD_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) install-bin DESTDIR=$(TARGET_DIR) PREFIX=/usr
+	$(MAKE1) $(LIBSERIAL_PPMD_MAKE_ENV) -C $(@D) install-bin DESTDIR=$(TARGET_DIR) PREFIX=/usr
 endef
 
 $(eval $(generic-package))
