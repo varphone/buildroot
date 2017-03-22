c3be06bf88c436ebe4ec54997bb890a6658db303
黄华锋 <varphone@qq.com>
2017-03-22 19:31:46 +0800

    vrs-qt: new package

---
+++ package/tdc/vrs-qt/vrs-qt.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# vrs-qt
+#
+################################################################################
+
+ifeq ($(BR2_PACKAGE_VRS_QT_CUSTOM_VERSION),y)
+VRS_QT_VERSION = $(call qstrip, $(BR2_PACKAGE_VRS_QT_CUSTOM_VERSION_VALUE))
+else
+VRS_QT_VERSION = 1.0.0
+endif
+VRS_QT_SOURCE = vrs-qt-$(VRS_QT_VERSION).tar.xz
+VRS_QT_SITE = https://10.0.2.2/cgit/rdst/vrs-qt.git/snapshot
+VRS_QT_STRIP_COMPONENTS = 1
+VRS_QT_INSTALL_STAGING = NO
+VRS_QT_DEPENDENCIES = libmal libxd_stream libpes-legacy libxd_player libxd_muxer mpp-lib qt xpr
+VRS_QT_LICENSE = GPLv2
+VRS_QT_LICENSE_FILES =
+
+VRS_QT_MAKE_ENV = \
+ CROSS_COMPILER_PREFIX=$(TARGET_CROSS)\
+ BUILD_DOCS=n\
+ BUILD_EXAMPLES=n\
+ BUILD_TESTS=n
+
+CP	= @cp
+MV	= @mv
+RMDIR	= @rmdir
+
+define VRS_QT_CONFIGURE_CMDS
+endef
+
+define VRS_QT_BUILD_CMDS
+	$(MAKE1) $(VRS_QT_MAKE_ENV) -C $(@D) clean all
+endef
+
+define VRS_QT_INSTALL_STAGING_CMDS
+endef
+
+define VRS_QT_INSTALL_TARGET_CMDS
+	$(MAKE1) $(VRS_QT_MAKE_ENV) -C $(@D) install-bin DESTDIR=$(TARGET_DIR) PREFIX=/usr
+endef
+
+$(eval $(generic-package))
