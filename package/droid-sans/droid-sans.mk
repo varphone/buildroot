1eb548f096951a8bec3e3f99fb69a334d2a59193
黄华锋 <varphone@qq.com>
2017-04-10 10:25:27 +0800

    droid-sans: Bump version to 1.0.1 and fix missing characters
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- package/droid-sans/droid-sans.mk
+++ package/droid-sans/droid-sans.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-DROID_SANS_VERSION = 1.0.0
+DROID_SANS_VERSION = 1.0.1
 DROID_SANS_SOURCE = droid-sans-fonts-ttf-$(DROID_SANS_VERSION).tar.xz
 DROID_SANS_SITE = https://10.0.2.2/git/rdst/binaries-release/raw/master
 DROID_SANS_STRIP_COMPONENTS = 0
