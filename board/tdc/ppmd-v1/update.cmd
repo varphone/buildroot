12e8d0ef0dbf3c91650c3b56d0cb89fda6090967
黄华锋 <varphone@qq.com>
2017-04-26 14:31:12 +0800

    ppmd-v1: add stmmac.hitoe=1 to boot environments
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- board/tdc/ppmd-v1/update.cmd
+++ board/tdc/ppmd-v1/update.cmd
@@ -3,6 +3,7 @@ setenv loadaddr 0x82000000
 setenv bootargs console=ttyAMA0,115200 mem=128M root=/dev/mtdblock4 ro rootfstype=squashfs panic=5
 setenv bootargs_base console=ttyAMA0,115200 mem=128M panic=5
 setenv mtdparts mtdparts=hinand:1M(boot),2M(reserved),4M(kernel),10M(initrd),30M(rootfs),8M(misc),-(cache)
+setenv netargs stmmac.hitoe=1
 setenv rootargs root=/dev/mtdblock4 rootfstype=squashfs overlayroot=/dev/ubi1_0:rw:ubifs
 tftp ${loadaddr} ppmd-v1/images/uImage
 nand erase 0x300000 0x400000
@@ -19,7 +20,7 @@ setenv rootfssize ${filesize}
 setenv loadinitrd 'nand read ${initrdaddr} 0x700000 0x400000'
 setenv loadkernel 'nand read ${loadaddr} 0x300000 0x400000'
 setenv setmac 'setenv mac eth0.mac=${ethaddr} eth1.mac=${eth1addr}'
-setenv setbootargs 'setenv bootargs ${bootargs_base} ${mtdparts} ${rootargs} initrd=${initrdaddr},0x${initrdsize} ${mac}'
+setenv setbootargs 'setenv bootargs ${bootargs_base} ${mtdparts} ${netargs} ${rootargs} initrd=${initrdaddr},0x${initrdsize} ${mac}'
 setenv bootcmd 'run setmac; run setbootargs; run loadinitrd; run loadkernel; bootm ${loadaddr}'
 sa
 
