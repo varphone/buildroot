a677ecae18b40dedb83995f9a70f06457e4b8411
黄华锋 <varphone@qq.com>
2017-06-15 16:51:59 +0800

    cvr-mil-v3: add tftp download checking and use mmcdev env in update-boot.cmd
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- board/tdc/cvr-mil-v3/update-boot.cmd
+++ board/tdc/cvr-mil-v3/update-boot.cmd
@@ -1,4 +1,3 @@
-mmc dev 1
-tftp ${loadaddr} cvr-mil-v3/images/boot.vfat
-mmc write ${loadaddr} 0x5000 0x20000
+mmc dev ${mmcdev}
+if tftp ${loadaddr} ${tftproot}boot.vfat; then echo "Boot updating ..."; mmc write ${loadaddr} 0x5000 0x20000; fi
 reset
