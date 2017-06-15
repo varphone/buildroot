d1f15da9f7d08d425287777e368e4e7f72c06453
黄华锋 <varphone@qq.com>
2017-06-15 16:50:12 +0800

    cvr-mil-v3: update display args in boot scripts
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- board/tdc/cvr-mil-v3/boot.cmd
+++ board/tdc/cvr-mil-v3/boot.cmd
@@ -1,7 +1,7 @@
 setenv loadfdt 'fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}'
 setenv loadimage 'fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image_file}'
 setenv loadinitrd 'fatload mmc ${mmcdev}:${mmcpart} ${initrd_addr} ${initrd_file}'
-setenv mmcargs 'run set_disp; setenv bootargs console=${console},${baudrate} ${smp} cma=320M root=${mmcroot} ${disp_args}'
+setenv mmcargs 'setenv bootargs console=${console},${baudrate} ${smp} cma=320M root=${mmcroot} ${display}'
 setenv mmcboot 'echo Booting from mmc ...; run mmcargs; if run loadimage; run loadinitrd; run loadfdt; then bootz ${loadaddr} ${initrd_addr}:${initrd_size} ${fdt_addr}; else echo WARN: Cannot boot from mmc; fi;'
 run mmcboot
 
