c4b320e8d57708849a9b6eab1790bcaa57bd59cf
黄华锋 <varphone@qq.com>
2017-06-16 16:34:53 +0800

    cvr-mil-v2: add consoleblank=0 to boot scripts
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- board/tdc/cvr-mil-v2/boot.cmd
+++ board/tdc/cvr-mil-v2/boot.cmd
@@ -1,7 +1,7 @@
 setenv loadfdt 'fatload mmc ${mmcdev}:${mmcpart} ${fdt_addr} ${fdt_file}'
 setenv loadimage 'fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${image_file}'
 setenv loadinitrd 'fatload mmc ${mmcdev}:${mmcpart} ${initrd_addr} ${initrd_file}'
-setenv mmcargs 'setenv bootargs console=${console},${baudrate} ${smp} cma=320M root=${mmcroot} ${display}'
+setenv mmcargs 'setenv bootargs console=${console},${baudrate} ${smp} cma=320M root=${mmcroot} ${display} consoleblank=0'
 setenv mmcboot 'echo Booting from mmc ...; run mmcargs; if run loadimage; run loadinitrd; run loadfdt; then bootz ${loadaddr} ${initrd_addr}:${initrd_size} ${fdt_addr}; else echo WARN: Cannot boot from mmc; fi;'
 run mmcboot
 
