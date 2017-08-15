b9851b38e70b1046c59b06f0ee287842934bd424
黄华锋 <varphone@qq.com>
2017-08-15 18:59:15 +0800

    cvr-mil-v2: reduce rootfs via pre-image.sh
    Signed-off-by: 黄华锋 <varphone@qq.com>

--- board/tdc/cvr-mil-v2/pre-image.sh
+++ board/tdc/cvr-mil-v2/pre-image.sh
@@ -1,2 +1,15 @@
-#!/bin/sh
+#!/bin/bash
 
+# Reduce the rootfs
+if [ -n "${TARGET_DIR}" ]; then
+	tput setaf 3
+	tput bold
+	echo "!!! Reducing the target filesystem: \"${TARGET_DIR}\" ..."
+	tput sgr0
+	pushd "${TARGET_DIR}"
+	rm /usr/lib/libVIVANT.fb.so
+	rm -rf usr/share/examples
+	rm -rf usr/share/ffmpeg/examples
+	rm -rf usr/share/imx-mm
+	popd
+fi
