61878034ef8bda0bd3895e0160453f44ae579c83
Varphone Wong <varphone@qq.com>
2018-01-10 14:32:39 +0800

    cvr-mil-v2-b: new board forked from cvr-mil-v2 for CVR-MIL-V2-B devices
    Signed-off-by: Varphone Wong <varphone@qq.com>

---
+++ board/tdc/cvr-mil-v2-b/pre-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Reduce the rootfs
+if [ -n "${TARGET_DIR}" ]; then
+	tput setaf 3
+	tput bold
+	echo "!!! Reducing the target filesystem: \"${TARGET_DIR}\" ..."
+	tput sgr0
+	pushd "${TARGET_DIR}"
+	rm usr/lib/libGAL.fb.so
+	rm usr/lib/libVIVANT.fb.so
+	rm -rf usr/share/examples
+	rm -rf usr/share/ffmpeg/examples
+	rm -rf usr/share/imx-mm
+	popd
+fi
