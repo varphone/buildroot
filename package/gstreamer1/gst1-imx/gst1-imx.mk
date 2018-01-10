f4c19c4e3a472755d4b1d9648646ccfa242be2f8
Varphone Wong <varphone@qq.com>
2018-01-10 08:48:14 +0800

    gst1-imx: Add libpavo support to custom patched version
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- package/gstreamer1/gst1-imx/gst1-imx.mk
+++ package/gstreamer1/gst1-imx/gst1-imx.mk
@@ -102,4 +102,11 @@ else
 GST1_IMX_CONF_OPTS += --disable-imxv4l2videosink
 endif
 
+ifeq ($(BR2_PACKAGE_LIBPAVO),y)
+# There's no --enable-pavo option
+GST1_IMX_DEPENDENCIES += libpavo
+else
+GST1_IMX_CONF_OPTS += --disable-pavo
+endif
+
 $(eval $(waf-package))
