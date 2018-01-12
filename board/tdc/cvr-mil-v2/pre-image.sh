9f1475bdd6e37c6d4c360fc9b4e9fab372a61114
Varphone Wong <varphone@qq.com>
2018-01-12 17:19:32 +0800

    cvr-mil-v2{a,b}: Fix bugs to remove files in pre-image.sh
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- board/tdc/cvr-mil-v2/pre-image.sh
+++ board/tdc/cvr-mil-v2/pre-image.sh
@@ -7,8 +7,8 @@ if [ -n "${TARGET_DIR}" ]; then
 	echo "!!! Reducing the target filesystem: \"${TARGET_DIR}\" ..."
 	tput sgr0
 	pushd "${TARGET_DIR}"
-	rm usr/lib/libGAL.fb.so
-	rm usr/lib/libVIVANT.fb.so
+	rm -f usr/lib/libGAL.fb.so
+	rm -f usr/lib/libVIVANTE.fb.so
 	rm -rf usr/share/examples
 	rm -rf usr/share/ffmpeg/examples
 	rm -rf usr/share/imx-mm
