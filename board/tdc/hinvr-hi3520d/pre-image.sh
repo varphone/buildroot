5841ba03e4351fd62f3337bbea6b3b4c03629280
Varphone Wong <varphone@qq.com>
2018-07-24 11:03:30 +0800

    hinvr-hi3520d: Reduce unused files in pre-image.sh
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- board/tdc/hinvr-hi3520d/pre-image.sh
+++ board/tdc/hinvr-hi3520d/pre-image.sh
@@ -20,3 +20,120 @@ if [ -d "${MISC_FS}" ]; then
 	tput sgr0
 fi
 
+# Reduce the rootfs
+if [ -n "${TARGET_DIR}" ]; then
+	tput setaf 3
+	tput bold
+	echo "!!! Reducing the target filesystem: \"${TARGET_DIR}\" ..."
+	tput sgr0
+	pushd "${TARGET_DIR}"
+	rm -f bin/chattr
+	rm -f bin/lsattr
+	rm -f sbin/badblocks
+	rm -f sbin/dumpe2fs
+	rm -f sbin/e2freefrag
+	rm -f sbin/e2undo
+	rm -f sbin/e4crypt
+	rm -f sbin/filefrag
+	rm -f sbin/logsave
+	rm -f sbin/resize2fs
+	rm -f sbin/tune2fs
+	rm -f usr/bin/aserver
+	rm -f usr/bin/fc-cache
+	rm -f usr/bin/fc-cat
+	rm -f usr/bin/fc-list
+	rm -f usr/bin/fc-match
+	rm -f usr/bin/fc-pattern
+	rm -f usr/bin/fc-query
+	rm -f usr/bin/fc-scan
+	rm -f usr/bin/fc-validate
+	rm -f usr/bin/gapplication
+	rm -f usr/bin/gdbus
+	rm -f usr/bin/gio
+	rm -f usr/bin/gio-querymodules
+	rm -f usr/bin/gresource
+	rm -f usr/bin/gsettings
+	rm -f usr/bin/gst-device-monitor-1.0
+	rm -f usr/bin/gst-discoverer-1.0
+	rm -f usr/bin/gst-play-1.0
+	rm -f usr/bin/hb-ot-shape-closure
+	rm -f usr/bin/hb-shape
+	rm -f usr/bin/hb-view
+	rm -f usr/bin/pango-view
+	rm -f usr/bin/pcregrep
+	rm -f usr/bin/pcretest
+	rm -f usr/bin/xmlwf
+	rm -rf usr/lib/gio
+	rm -rf usr/lib/gstreamer-1.0
+	rm -rf usr/lib/qt
+	rm -f usr/lib/libaec.so
+	rm -f usr/lib/libanr.so
+	rm -f usr/lib/libjpeg.so
+	rm -f usr/lib/libjpeg6b.so
+	rm -f usr/lib/libmem.so
+	rm -f usr/lib/libmpi.so
+	rm -f usr/lib/libresampler.so
+	rm -f usr/lib/libtde.so
+	rm -f usr/lib/libVoiceEngine.so
+	rm -f usr/lib/libvqev2.so
+	rm -rf usr/share/alsa
+	rm -rf usr/share/ffmpeg
+	rm -rf usr/share/gettext
+	rm -rf usr/share/glib-2.0
+	rm -rf usr/share/gst-plugins-base
+	rm -rf usr/share/gstreamer-1.0
+	# Remove unused timezone
+	rm -rf usr/share/zoneinfo/uclibc/Africa
+	rm -rf usr/share/zoneinfo/uclibc/America
+	rm -rf usr/share/zoneinfo/uclibc/Antarctica
+	rm -rf usr/share/zoneinfo/uclibc/Arctic
+	rm -rf usr/share/zoneinfo/uclibc/Atlantic
+	rm -rf usr/share/zoneinfo/uclibc/Australia
+	rm -rf usr/share/zoneinfo/uclibc/Brazil
+	rm -rf usr/share/zoneinfo/uclibc/Canada
+	rm -rf usr/share/zoneinfo/uclibc/Chile
+	rm -rf usr/share/zoneinfo/uclibc/Etc
+	rm -rf usr/share/zoneinfo/uclibc/Europe
+	rm -rf usr/share/zoneinfo/uclibc/Indian
+	rm -rf usr/share/zoneinfo/uclibc/Mexico
+	rm -rf usr/share/zoneinfo/uclibc/Pacific
+	rm -rf usr/share/zoneinfo/uclibc/US
+	pushd usr/share/zoneinfo/uclibc/Asia
+	find . -type f ! -name Chongqing ! -name Shanghai -delete
+	popd
+	# Remove unused live555 progs
+	rm -f usr/bin/live555MediaServer
+	rm -f usr/bin/live555ProxyServer
+	rm -f usr/bin/MPEG2TransportStreamIndexer
+	rm -f usr/bin/openRTSP
+	rm -f usr/bin/playSIP
+	rm -f usr/bin/registerRTSPStream
+	rm -f usr/bin/sapWatch
+	rm -f usr/bin/testAMRAudioStreamer
+	rm -f usr/bin/testDVVideoStreamer
+	rm -f usr/bin/testH264VideoStreamer
+	rm -f usr/bin/testH264VideoToTransportStream
+	rm -f usr/bin/testH265VideoStreamer
+	rm -f usr/bin/testH265VideoToTransportStream
+	rm -f usr/bin/testMKVStreamer
+	rm -f usr/bin/testMP3Receiver
+	rm -f usr/bin/testMP3Streamer
+	rm -f usr/bin/testMPEG1or2AudioVideoStreamer
+	rm -f usr/bin/testMPEG1or2ProgramToTransportStream
+	rm -f usr/bin/testMPEG1or2Splitter
+	rm -f usr/bin/testMPEG1or2VideoReceiver
+	rm -f usr/bin/testMPEG1or2VideoStreamer
+	rm -f usr/bin/testMPEG2TransportReceiver
+	rm -f usr/bin/testMPEG2TransportStreamer
+	rm -f usr/bin/testMPEG2TransportStreamTrickPlay
+	rm -f usr/bin/testMPEG4VideoStreamer
+	rm -f usr/bin/testOggStreamer
+	rm -f usr/bin/testOnDemandRTSPServer
+	rm -f usr/bin/testRelay
+	rm -f usr/bin/testReplicator
+	rm -f usr/bin/testRTSPClient
+	rm -f usr/bin/testWAVAudioStreamer
+	rm -f usr/bin/vobStreamer
+	popd
+fi
+
