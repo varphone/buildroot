d2279c38ce673abfd98bf55bde1da62abf2aa91f
Varphone Wong <varphone@qq.com>
2018-08-12 22:03:09 +0800

    hinvr-hi3520d: Disabled lz4 in fakeroot.sh
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- board/tdc/hinvr-hi3520d/fakeroot.sh
+++ board/tdc/hinvr-hi3520d/fakeroot.sh
@@ -21,7 +21,7 @@ if [[ "${BUILD_INITRD}" = "y" ]]; then
 	echo "### Cleanup initramfs if exists"
 	rm "${BINARIES_DIR}/initrd.cpio"
 	rm "${BINARIES_DIR}/initrd.cpio.gz"
-	rm "${BINARIES_DIR}/initrd.cpio.lz4"
+	#rm "${BINARIES_DIR}/initrd.cpio.lz4"
 	rm "${BINARIES_DIR}/initrd.cpio.lzo"
 	rm "${BINARIES_DIR}/initrd.cpio.xz"
 	rm -rf "${BINARIES_DIR}/initramfs"
@@ -45,8 +45,8 @@ if [[ "${BUILD_INITRD}" = "y" ]]; then
 	echo "### Compress with gzip ..."
 	gzip -c "${BINARIES_DIR}/initrd.cpio" > "${BINARIES_DIR}/initrd.cpio.gz"
 
-	echo "### Compress with lz4 ..."
-	lz4 -9 -f "${BINARIES_DIR}/initrd.cpio" "${BINARIES_DIR}/initrd.cpio.lz4"
+	#echo "### Compress with lz4 ..."
+	#lz4 -9 -f "${BINARIES_DIR}/initrd.cpio" "${BINARIES_DIR}/initrd.cpio.lz4"
 
 	echo "### Compress with lzo ..."
 	lzop -9 -o "${BINARIES_DIR}/initrd.cpio.lzo" "${BINARIES_DIR}/initrd.cpio"
