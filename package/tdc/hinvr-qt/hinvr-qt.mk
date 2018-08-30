3d1f4d3efbca6a4aa8082c2aa6bfaed3c159309b
Varphone Wong <varphone@qq.com>
2018-08-30 22:15:16 +0800

    hinvr-qt: Bump version to 1.0.9
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- package/tdc/hinvr-qt/hinvr-qt.mk
+++ package/tdc/hinvr-qt/hinvr-qt.mk
@@ -7,7 +7,7 @@
 ifeq ($(BR2_PACKAGE_HINVR_QT_CUSTOM_VERSION),y)
 HINVR_QT_VERSION = $(call qstrip, $(BR2_PACKAGE_HINVR_QT_CUSTOM_VERSION_VALUE))
 else
-HINVR_QT_VERSION = 1.0.0
+HINVR_QT_VERSION = 1.0.9
 endif
 HINVR_QT_SOURCE = hinvr-qt-$(HINVR_QT_VERSION).tar.xz
 HINVR_QT_SITE = https://10.0.2.2/cgit/rdst/hinvr-qt.git/snapshot
@@ -34,6 +34,8 @@ define HINVR_QT_INSTALL_STAGING_CMDS
 endef
 
 define HINVR_QT_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/fonts
+	$(INSTALL) -m 0644 $(@D)/fonts/Ubunoto-Mono-SC.tff $(TARGET_DIR)/usr/share/fonts/
 	$(INSTALL) -m 0755 $(@D)/src/hinvr-qt $(TARGET_DIR)/usr/bin/
 endef
 
