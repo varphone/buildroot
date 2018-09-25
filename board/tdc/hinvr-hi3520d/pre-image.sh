30adeed86c21d7b2945945b764d0ee837b6796fa
Varphone Wong <varphone@qq.com>
2018-09-25 20:46:12 +0800

    hinvr-hi3520d: new board backported to gcc-4.4 + uclibc-0.9.32.1 from 2018.08-hinvr
    Signed-off-by: Varphone Wong <varphone@qq.com>

---
+++ board/tdc/hinvr-hi3520d/pre-image.sh
@@ -0,0 +1,154 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname $(readlink -f "$0"))
+TAR="tar --ignore-failed-read"
+
+FULL_BUILD=n
+MINI_BUILD=y
+
+while true; do
+	case "$1" in
+		-f | --full ) FULL_BUILD=y MINI_BUILD=n; shift ;;
+		-m | --mini ) FULL_BUILD=n MINI_BUILD=y; shift ;;
+		-- ) shift; break ;;
+		* ) break ;;
+	esac
+done
+
+# Gen misc ubifs image
+MISC_FS="${SCRIPT_DIR}/misc_fs"
+MISC_UBIFS="${BINARIES_DIR}/misc.ubifs"
+
+if [ -d "${MISC_FS}" ]; then
+	tput setaf 3
+	echo "`tput bold`### Make misc ubifs image ...`tput sgr0`"
+	tput setaf 6
+
+	# 64 LEBs = 8MB
+	mkfs.ubifs -F -m 2048 -e 126976 -c 64 -U \
+		-r "${MISC_FS}" \
+		"${MISC_UBIFS}"
+
+	tput sgr0
+fi
+
+# Reduce the rootfs
+if [ -n "${TARGET_DIR}" -a "x${MINI_BUILD}" == "xy" ]; then
+	tput setaf 3
+	tput bold
+	echo "!!! Reducing the target filesystem: \"${TARGET_DIR}\" ..."
+	tput sgr0
+	pushd "${TARGET_DIR}"
+	rm -f bin/chattr
+	rm -f bin/lsattr
+	rm -f sbin/badblocks
+	rm -f sbin/dumpe2fs
+	rm -f sbin/e2freefrag
+	rm -f sbin/e2undo
+	rm -f sbin/e4crypt
+	rm -f sbin/filefrag
+	rm -f sbin/logsave
+	rm -f sbin/resize2fs
+	rm -f sbin/tune2fs
+	rm -f usr/bin/aserver
+	rm -f usr/bin/fc-cache
+	rm -f usr/bin/fc-cat
+	rm -f usr/bin/fc-list
+	rm -f usr/bin/fc-match
+	rm -f usr/bin/fc-pattern
+	rm -f usr/bin/fc-query
+	rm -f usr/bin/fc-scan
+	rm -f usr/bin/fc-validate
+	rm -f usr/bin/gapplication
+	rm -f usr/bin/gdbus
+	rm -f usr/bin/gio
+	rm -f usr/bin/gio-querymodules
+	rm -f usr/bin/gresource
+	rm -f usr/bin/gsettings
+	rm -f usr/bin/gst-device-monitor-1.0
+	rm -f usr/bin/gst-discoverer-1.0
+	rm -f usr/bin/gst-play-1.0
+	rm -f usr/bin/hb-ot-shape-closure
+	rm -f usr/bin/hb-shape
+	rm -f usr/bin/hb-view
+	rm -f usr/bin/pango-view
+	rm -f usr/bin/pcregrep
+	rm -f usr/bin/pcretest
+	rm -f usr/bin/xmlwf
+	rm -f usr/bin/xmlcatalog
+	rm -f usr/bin/xmllint
+	rm -rf usr/lib/gio
+	rm -rf usr/lib/gstreamer-1.0
+	rm -rf usr/lib/qt
+	rm -f usr/lib/libaec.so
+	rm -f usr/lib/libanr.so
+	rm -f usr/lib/libjpeg.so
+	rm -f usr/lib/libjpeg6b.so
+	rm -f usr/lib/libmem.so
+	rm -f usr/lib/libmpi.so
+	rm -f usr/lib/libresampler.so
+	rm -f usr/lib/libtde.so
+	rm -f usr/lib/libVoiceEngine.so
+	rm -f usr/lib/libvqev2.so
+	rm -rf usr/libexec/lzo
+	rm -rf usr/share/alsa
+	rm -rf usr/share/ffmpeg
+	rm -rf usr/share/gettext
+	rm -rf usr/share/glib-2.0
+	rm -rf usr/share/gst-plugins-base
+	rm -rf usr/share/gstreamer-1.0
+	# Remove unused timezone
+	rm -rf usr/share/zoneinfo/uclibc/Africa
+	rm -rf usr/share/zoneinfo/uclibc/America
+	rm -rf usr/share/zoneinfo/uclibc/Antarctica
+	rm -rf usr/share/zoneinfo/uclibc/Arctic
+	rm -rf usr/share/zoneinfo/uclibc/Atlantic
+	rm -rf usr/share/zoneinfo/uclibc/Australia
+	rm -rf usr/share/zoneinfo/uclibc/Brazil
+	rm -rf usr/share/zoneinfo/uclibc/Canada
+	rm -rf usr/share/zoneinfo/uclibc/Chile
+	rm -rf usr/share/zoneinfo/uclibc/Etc
+	rm -rf usr/share/zoneinfo/uclibc/Europe
+	rm -rf usr/share/zoneinfo/uclibc/Indian
+	rm -rf usr/share/zoneinfo/uclibc/Mexico
+	rm -rf usr/share/zoneinfo/uclibc/Pacific
+	rm -rf usr/share/zoneinfo/uclibc/US
+	pushd usr/share/zoneinfo/uclibc/Asia
+	find . -type f ! -name Chongqing ! -name Shanghai -delete
+	popd
+	# Remove unused live555 progs
+	rm -f usr/bin/live555MediaServer
+	rm -f usr/bin/live555ProxyServer
+	rm -f usr/bin/MPEG2TransportStreamIndexer
+	rm -f usr/bin/openRTSP
+	rm -f usr/bin/playSIP
+	rm -f usr/bin/registerRTSPStream
+	rm -f usr/bin/sapWatch
+	rm -f usr/bin/testAMRAudioStreamer
+	rm -f usr/bin/testDVVideoStreamer
+	rm -f usr/bin/testH264VideoStreamer
+	rm -f usr/bin/testH264VideoToTransportStream
+	rm -f usr/bin/testH265VideoStreamer
+	rm -f usr/bin/testH265VideoToTransportStream
+	rm -f usr/bin/testMKVStreamer
+	rm -f usr/bin/testMP3Receiver
+	rm -f usr/bin/testMP3Streamer
+	rm -f usr/bin/testMPEG1or2AudioVideoStreamer
+	rm -f usr/bin/testMPEG1or2ProgramToTransportStream
+	rm -f usr/bin/testMPEG1or2Splitter
+	rm -f usr/bin/testMPEG1or2VideoReceiver
+	rm -f usr/bin/testMPEG1or2VideoStreamer
+	rm -f usr/bin/testMPEG2TransportReceiver
+	rm -f usr/bin/testMPEG2TransportStreamer
+	rm -f usr/bin/testMPEG2TransportStreamTrickPlay
+	rm -f usr/bin/testMPEG4VideoStreamer
+	rm -f usr/bin/testOggStreamer
+	rm -f usr/bin/testOnDemandRTSPServer
+	rm -f usr/bin/testRelay
+	rm -f usr/bin/testReplicator
+	rm -f usr/bin/testRTSPClient
+	rm -f usr/bin/testWAVAudioStreamer
+	rm -f usr/bin/vobStreamer
+	popd
+fi
+
---
+++ board/tdc/hinvr-hi3520d/qt-4.8-qconfig-lite.h
@@ -0,0 +1,295 @@
+/* Crypto */
+#ifndef QT_NO_OPENSSL
+#  define QT_NO_OPENSSL
+#endif
+
+/* Dialogs */
+#ifndef QT_NO_COLORDIALOG
+#  define QT_NO_COLORDIALOG
+#endif
+#ifndef QT_NO_FILEDIALOG
+#  define QT_NO_FILEDIALOG
+#endif
+#ifndef QT_NO_FILESYSTEMMODEL
+#  define QT_NO_FILESYSTEMMODEL
+#endif
+#ifndef QT_NO_FONTDIALOG
+#  define QT_NO_FONTDIALOG
+#endif
+#ifndef QT_NO_INPUTDIALOG
+#  define QT_NO_INPUTDIALOG
+#endif
+#ifndef QT_NO_PRINTDIALOG
+#  define QT_NO_PRINTDIALOG
+#endif
+#ifndef QT_NO_PRINTPREVIEWDIALOG
+#  define QT_NO_PRINTPREVIEWDIALOG
+#endif
+#ifndef QT_NO_PROGRESSDIALOG
+#  define QT_NO_PROGRESSDIALOG
+#endif
+#ifndef QT_NO_TABDIALOG
+#  define QT_NO_TABDIALOG
+#endif
+#ifndef QT_NO_WIZARD
+#  define QT_NO_WIZARD
+#endif
+
+/* File I/O */
+
+/* Fonts */
+#ifndef QT_NO_QWS_QPF2
+#  define QT_NO_QWS_QPF2
+#endif
+
+/* Images */
+#ifndef QT_NO_IMAGEFORMAT_PPM
+#  define QT_NO_IMAGEFORMAT_PPM
+#endif
+#ifndef QT_NO_IMAGEFORMAT_XBM
+#  define QT_NO_IMAGEFORMAT_XBM
+#endif
+#ifndef QT_NO_IMAGE_HEURISTIC_MASK
+#  define QT_NO_IMAGE_HEURISTIC_MASK
+#endif
+#ifndef QT_NO_IMAGE_TEXT
+#  define QT_NO_IMAGE_TEXT
+#endif
+#ifndef QT_NO_MOVIE
+#  define QT_NO_MOVIE
+#endif
+
+/* Internationalization */
+
+/* ItemViews */
+#ifndef QT_NO_DIRMODEL
+#  define QT_NO_DIRMODEL
+#endif
+#ifndef QT_NO_FILEICONPROVIDER
+#  define QT_NO_FILEICONPROVIDER
+#endif
+#ifndef QT_NO_LISTVIEW
+#  define QT_NO_LISTVIEW
+#endif
+#ifndef QT_NO_LISTWIDGET
+#  define QT_NO_LISTWIDGET
+#endif
+#ifndef QT_NO_TREEVIEW
+#  define QT_NO_TREEVIEW
+#endif
+#ifndef QT_NO_TREEWIDGET
+#  define QT_NO_TREEWIDGET
+#endif
+
+/* Kernel */
+#ifndef QT_NO_CLIPBOARD
+#  define QT_NO_CLIPBOARD
+#endif
+#ifndef QT_NO_DRAGANDDROP
+#  define QT_NO_DRAGANDDROP
+#endif
+#ifndef QT_NO_EFFECTS
+#  define QT_NO_EFFECTS
+#endif
+#ifndef QT_NO_SESSIONMANAGER
+#  define QT_NO_SESSIONMANAGER
+#endif
+#ifndef QT_NO_SOUND
+#  define QT_NO_SOUND
+#endif
+#ifndef QT_NO_TABLETEVENT
+#  define QT_NO_TABLETEVENT
+#endif
+#ifndef QT_NO_TEXTHTMLPARSER
+#  define QT_NO_TEXTHTMLPARSER
+#endif
+#ifndef QT_NO_WHEELEVENT
+#  define QT_NO_WHEELEVENT
+#endif
+
+/* Networking */
+#ifndef QT_NO_BEARERMANAGEMENT
+#  define QT_NO_BEARERMANAGEMENT
+#endif
+#ifndef QT_NO_COP
+#  define QT_NO_COP
+#endif
+#ifndef QT_NO_FTP
+#  define QT_NO_FTP
+#endif
+#ifndef QT_NO_HTTP
+#  define QT_NO_HTTP
+#endif
+#ifndef QT_NO_NETWORKPROXY
+#  define QT_NO_NETWORKPROXY
+#endif
+
+/* Painting */
+#ifndef QT_NO_PICTURE
+#  define QT_NO_PICTURE
+#endif
+#ifndef QT_NO_PRINTER
+#  define QT_NO_PRINTER
+#endif
+#ifndef QT_NO_CUPS
+#  define QT_NO_CUPS
+#endif
+
+/* Text */
+#ifndef QT_NO_SYNTAXHIGHLIGHTER
+#  define QT_NO_SYNTAXHIGHLIGHTER
+#endif
+#ifndef QT_NO_TEXTHTMLPARSER
+#  define QT_NO_TEXTHTMLPARSER
+#endif
+#ifndef QT_NO_TEXTODFWRITER
+#  define QT_NO_TEXTODFWRITER
+#endif
+
+/* Qt for Embedded Linux */
+#ifndef QT_NO_QWS_MULTIPROCESS
+#  define QT_NO_QWS_MULTIPROCESS
+#endif
+#ifndef QT_NO_QWS_SOUNDSERVER
+#  define QT_NO_QWS_SOUNDSERVER
+#endif
+#ifndef QT_NO_SXE
+#  define QT_NO_SXE
+#endif
+
+/* SVG */
+#ifndef QT_NO_SVG
+#  define QT_NO_SVG
+#endif
+#ifndef QT_NO_GRAPHICSSVGITEM
+#  define QT_NO_GRAPHICSSVGITEM
+#endif
+#ifndef QT_NO_SVGGENERATOR
+#  define QT_NO_SVGGENERATOR
+#endif
+#ifndef QT_NO_SVGRENDERER
+#  define QT_NO_SVGRENDERER
+#endif
+#ifndef QT_NO_SVGWIDGET
+#  define QT_NO_SVGWIDGET
+#endif
+
+/* Styles */
+#ifndef QT_NO_STYLE_MOTIF
+#  define QT_NO_STYLE_MOTIF
+#endif
+#ifndef QT_NO_STYLE_CDE
+#  define QT_NO_STYLE_CDE
+#endif
+#ifndef QT_NO_STYLE_PLASTIQUE
+#  define QT_NO_STYLE_PLASTIQUE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSCE
+#  define QT_NO_STYLE_WINDOWSCE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSMOBILE
+#  define QT_NO_STYLE_WINDOWSMOBILE
+#endif
+#ifndef QT_NO_STYLE_WINDOWSVISTA
+#  define QT_NO_STYLE_WINDOWSVISTA
+#endif
+#ifndef QT_NO_STYLE_WINDOWSXP
+#  define QT_NO_STYLE_WINDOWSXP
+#endif
+
+/* Utilities */
+#ifndef QT_NO_ACCESSIBILITY
+#  define QT_NO_ACCESSIBILITY
+#endif
+#ifndef QT_NO_DESKTOPSERVICES
+#  define QT_NO_DESKTOPSERVICES
+#endif
+#ifndef QT_NO_SCRIPT
+#  define QT_NO_SCRIPT
+#endif
+#ifndef QT_NO_SYSTEMTRAYICON
+#  define QT_NO_SYSTEMTRAYICON
+#endif
+#ifndef QT_NO_UNDOCOMMAND
+#  define QT_NO_UNDOCOMMAND
+#endif
+#ifndef QT_NO_UNDOGROUP
+#  define QT_NO_UNDOGROUP
+#endif
+#ifndef QT_NO_UNDOSTACK
+#  define QT_NO_UNDOSTACK
+#endif
+#ifndef QT_NO_UNDOVIEW
+#  define QT_NO_UNDOVIEW
+#endif
+#ifndef QT_NO_GESTURES
+#  define QT_NO_GESTURES
+#endif
+
+/* Widgets */
+#ifndef QT_NO_LCDNUMBER
+#  define QT_NO_LCDNUMBER
+#endif
+#ifndef QT_NO_FONTCOMBOBOX
+#  define QT_NO_FONTCOMBOBOX
+#endif
+#ifndef QT_NO_MAINWINDOW
+#  define QT_NO_MAINWINDOW
+#endif
+#ifndef QT_NO_DOCKWIDGET
+#  define QT_NO_DOCKWIDGET
+#endif
+#ifndef QT_NO_TOOLBAR
+#  define QT_NO_TOOLBAR
+#endif
+#ifndef QT_NO_MENUBAR
+#  define QT_NO_MENUBAR
+#endif
+#ifndef QT_NO_WORKSPACE
+#  define QT_NO_WORKSPACE
+#endif
+#ifndef QT_NO_SIZEGRIP
+#  define QT_NO_SIZEGRIP
+#endif
+#ifndef QT_NO_DIAL
+#  define QT_NO_DIAL
+#endif
+#ifndef QT_NO_GRAPHICSVIEW
+#  define QT_NO_GRAPHICSVIEW
+#endif
+#ifndef QT_NO_PRINTPREVIEWWIDGET
+#  define QT_NO_PRINTPREVIEWWIDGET
+#endif
+#ifndef QT_NO_MDIAREA
+#  define QT_NO_MDIAREA
+#endif
+#ifndef QT_NO_SYNTAXHIGHLIGHTER
+#  define QT_NO_SYNTAXHIGHLIGHTER
+#endif
+#ifndef QT_NO_TEXTBROWSER
+#  define QT_NO_TEXTBROWSER
+#endif
+#ifndef QT_NO_SPLASHSCREEN
+#  define QT_NO_SPLASHSCREEN
+#endif
+#ifndef QT_NO_STATUSBAR
+#  define QT_NO_STATUSBAR
+#endif
+#ifndef QT_NO_STATUSTIP
+#  define QT_NO_STATUSTIP
+#endif
+#ifndef QT_NO_TOOLBUTTON
+#  define QT_NO_TOOLBUTTON
+#endif
+#ifndef QT_NO_TABBAR
+#  define QT_NO_TABBAR
+#endif
+#ifndef QT_NO_TOOLBOX
+#  define QT_NO_TOOLBOX
+#endif
+#ifndef QT_NO_WHATSTHIS
+#  define QT_NO_WHATSTHIS
+#endif
+#ifndef QT_NO_TOOLTIP
+#  define QT_NO_TOOLTIP
+#endif
