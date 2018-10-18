d0460f2c52ccdfe48bc3dded6c3569de9bc17d18
Varphone Wong <varphone@qq.com>
2018-10-18 15:29:59 +0800

    cvr-mil-v2-a: Add GST_EVR_OVL_MASK=13 to /etc/profile.d/qt.sh
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- board/tdc/cvr-mil-v2-a/rootfs_overlay/etc/profile.d/qt.sh
+++ board/tdc/cvr-mil-v2-a/rootfs_overlay/etc/profile.d/qt.sh
@@ -3,4 +3,5 @@ export QT_EGLFS_IMX6_NO_FB_MULTI_BUFFER=0
 export QT_QPA_EGLFS_FB=/dev/fb1
 export QT_QPA_EVDEV_KEYBOARD_PARAMETERS=Linuxinput:/dev/input/event0:keymap=/etc/cvr/atm88pa.qmap
 export QT_QPA_FONTDIR=/usr/share/fonts
+export GST_EVR_OVL_MASK=13
 
