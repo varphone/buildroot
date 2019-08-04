7c7123d6f2271994a9f3d528a6e6b7cd0e57b3a1
Romain Naour <romain.naour@smile.fr>
2019-08-04 20:54:08 +0200

    package/python3-mako: new package
    host-python3-mako will be used by python 3 only script used in the
    build system of mesa3d package.
    
    There is no way to force building host-oython-mako for python2 and
    python3 [1].
    
    [1] http://patchwork.ozlabs.org/patch/1084248/
    
    Signed-off-by: Romain Naour <romain.naour@smile.fr>
    Signed-off-by: Bernd Kuhls <bernd.kuhls@t-online.de>
    Tested-By: Cengiz Can <cengiz@kernel.wtf>
    Tested-By: Cengiz Can <cengiz@kernel.wtf>

---
+++ package/python3-mako/python3-mako.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# python3-mako
+#
+################################################################################
+
+# Please keep in sync with
+# package/python-mako/python-mako.mk
+PYTHON3_MAKO_VERSION = 1.0.6
+PYTHON3_MAKO_SOURCE = Mako-$(PYTHON_MAKO_VERSION).tar.gz
+PYTHON3_MAKO_SITE = https://pypi.python.org/packages/56/4b/cb75836863a6382199aefb3d3809937e21fa4cb0db15a4f4ba0ecc2e7e8e
+PYTHON3_MAKO_SETUP_TYPE = setuptools
+PYTHON3_MAKO_LICENSE = MIT
+PYTHON3_MAKO_LICENSE_FILES = LICENSE
+HOST_PYTHON3_MAKO_DL_SUBDIR = python-mako
+HOST_PYTHON3_MAKO_NEEDS_HOST_PYTHON = python3
+
+# In host build, setup.py tries to download markupsafe if it is not installed
+HOST_PYTHON3_MAKO_DEPENDENCIES = host-python-markupsafe
+
+$(eval $(host-python-package))
