025ec532c888289acbc8ce5415829b6fd7f2dcf3
Varphone Wong <varphone@qq.com>
2021-02-27 17:15:04 +0800

    zylaser: Add misc partition supports
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- board/full-v/zylaser/pre-image.sh
+++ board/full-v/zylaser/pre-image.sh
@@ -3,5 +3,10 @@
 # Fix missing /init
 ln -sf /sbin/init ${TARGET_DIR}/init
 
+# Generate the misc.ext2 image
+mkdir -p ${BINARIES_DIR}/misc
+cp -aprv board/full-v/zylaser/misc_overlay/* ${BINARIES_DIR}/misc/
+genext2fs -d ${BINARIES_DIR}/misc -b 4096 ${BINARIES_DIR}/misc.ext2
+
 exit $?
 
